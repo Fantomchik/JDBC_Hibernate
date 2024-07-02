48ba6c1568cf385b7ac6eafe41ea5b3b52bacc55
Fantomchik <58704419+Fantomchik@users.noreply.github.com>
2024-07-02 11:26:55 +0300

    Add files via upload

---
+++ Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,34 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Dire", "Wolf", (byte) 150);
+        userService.saveUser("Tyrion", "Lannister", (byte) 30);
+        userService.saveUser("Daenerys", "Targaryen", (byte) 28);
+        userService.saveUser("John", "Snow", (byte) 25);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+        for (User user: userService.getAllUsers()) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
+
+    }
+}
